a31a8df955751108dc1f98781cca116677ddf3a1
simon despres <simondprs62790@hotmail.fr>
2023-10-17 12:44:25 +0200

    routage fonctionnel + toute les pages créer dans un dossier page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
+/**
+ * The external imports
+ */
+import { Routes, Route } from "react-router-dom";
 
+/**
+ * The internal imports
+ */
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
+import Portefolio from "./components/pages/Portefolio";
+import NotFound from "./components/pages/NotFound";
+
+function App() {
   return (
-    <>
-      <h1 className="bg-red-300">Hello world</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/portefolio" element={<Portefolio />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/**
+ * The external imports
+ */
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+/**
+ * The internal imports
+ */
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
